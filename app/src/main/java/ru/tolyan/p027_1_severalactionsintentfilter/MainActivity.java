b60ec391121d149b8f87c5ae3a2bc9a8df0bbda5
Tolyan <Tolyan@mail.ru>
2016-03-06 15:35:31 +0000

    Initial commit

---
+++ app/src/main/java/ru/tolyan/p027_1_severalactionsintentfilter/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.tolyan.p027_1_severalactionsintentfilter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
